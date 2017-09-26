1c0693018a91a1d4f3269d7b3542be6097a2cf91
Paulo Cezar de Queiroz Hermida Filho <pcqhf@icomp.ufam.edu.br>
2017-09-26 14:53:01 -0400

    add gitignore

--- tp1/main.c
+++ tp1/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+struct Processo {
+	int pid;
+	int tempoIngresso;
+	int duracao;
+	int prioridade;
+	int tipo;
+	struct Processo* prox;
+};
+
 int main(){
 
 	printf("Hello World");
