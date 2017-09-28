a06d19b26290cee8f6858dcd5e700035a23edb8a
U-TRT11\600113 <600113@MNSDPESSOAL66.trt11.intranet>
2017-09-28 10:45:05 -0400

    Adcionado header

--- tp1/main.c
+++ tp1/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "file.h"
+
 
 struct Processo {
 	int pid;
@@ -9,9 +12,36 @@ struct Processo {
 	struct Processo* prox;
 };
 
-int main(){
+int main(void) {
+	//FILE* f;
+	//f = fopen("processos.txt", "r");
+	
+	
 
-	printf("Hello World teste commit");
+	printf("Hello World");
 	return 0;
+}
 
+struct Processo* init(int pid, int tempoIngresso, int duracao, int prioridade, int tipo){
+	struct Processo* proc;
+ 	proc = (struct Processo*)malloc(sizeof(struct Processo)); 
+  	if (proc == NULL) {
+       	 printf("Erro!");
+       	 exit(1);
+   	 };
+    	
+	proc->pid = pid;
+	proc->tempoIngresso = tempoIngresso;
+	proc->duracao = duracao;
+	proc->prioridade = prioridade;
+	proc->tipo = tipo;
+	proc->prox = NULL;
+	return(proc);
+}
+
+void listaProcessos(struct Processo* proc){
+	struct Processo* t = proc;
+	while(t != NULL){
+		printf(t->pid, t->tempoIngresso, t->duracao, t->prioridade, t->tipo);
+	}	
 }
