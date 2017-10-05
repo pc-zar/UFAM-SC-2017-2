5247609514e1c33a67949dbaa6d9697da2afe65d
U-TRT11\600113 <600113@MNSDPESSOAL66.trt11.intranet>
2017-10-05 12:39:59 -0400

    sdfsdf

--- tp2/main.c
+++ tp2/main.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define MAX_CLIENTE 25
+
+sem_t waitingRoom;
 
 int main(){
 	printf("Hello World");
