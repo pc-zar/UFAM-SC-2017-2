07fbadfcba5f92dbcc6184e02661c98c20aff3c5
U-TRT11\600113 <600113@MNSDPESSOAL66.trt11.intranet>
2017-10-11 11:38:39 -0400

    começo das 3 filas

--- tp2/main.c
+++ tp2/main.c
@@ -1,14 +1,45 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <semaphore.h>
 
-#define MAX_CLIENTE 25
-
-sem_t waitingRoom;
+typedef struct NodeCliente{
+	int categoria;
+	int tempo;
+	struct NodeCliente *prox;
+} nodeCliente_t ;
 
 int main(){
-	printf("Hello World");
+	initFilas();
 	return 0;
 }
+
+void initFilas(){
+	nodeCliente_t *oficial, *sgt, *cabo;
+	int i = 0;
+	int catTemp = 0;
+	int tempoTemp = 0;
+	while(i < 100){
+		catTemp = rand_range(0, 3);
+		switch(catTemp){
+			case 1:
+				oficial->categoria = catTemp;
+				oficial->tempo = rand_range(4, 6);
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 0:
+				break;
+		}
+		printf("\n");
+		i++;
+	}	
+}
+
+int rand_range(int min_n, int max_n){
+	return rand() % (max_n - min_n + 1) + min_n;
+}
+
+
+
+
+
