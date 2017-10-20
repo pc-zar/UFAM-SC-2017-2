65612b1dc5322343b8efbed02866861c4f83ca39
U-TRT11\600113 <600113@MNSDPESSOAL66.trt11.intranet>
2017-10-20 13:50:25 -0400

    implementacao funcao insere node

--- tp2/barbeariaRecrutaZero.c
+++ tp2/barbeariaRecrutaZero.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <pthread.h>
+#include <semaphore.h>
 
-#define MAX_CLIENTES 1000
+#define MAX_CLIENTES 10
 #define MAX_BARBEARIA 20
 #define MAX_BARBEIROS 1
+
+sem_t oficial, sgt, cabo;
 
 typedef struct NodeCliente{
 	int categoria;
@@ -16,11 +19,12 @@ typedef struct NodeCliente{
 //prototypes
 nodeCliente_t *initFilaEspera();
 void printLista(nodeCliente_t *head);
+nodeCliente_t *appendCliente(nodeCliente_t *head, int catTemp, int tempoTemp);
 int rand_range(int min_n, int max_n);
 
 int main(void){
 	srand(time(NULL));
-	pthread_t clientes[MAX_CLEINTES];
+	pthread_t clientes[MAX_CLIENTES];
 	pthread_t barbeiros[MAX_BARBEIROS];
 
 	nodeCliente_t *filaEspera, *oficial, *sgt, *cabo;
@@ -30,6 +34,8 @@ int main(void){
 	oficial = NULL;
 	sgt = NULL;
 	cabo = NULL;
+
+	printLista(filaEspera);
 
 	return 0;
 }
@@ -54,7 +60,8 @@ nodeCliente_t *initFilaEspera(){
 				tempoTemp = 0;
 				break;
 		}
-
+		
+	/*	
 		if(entrada == NULL){
 			entrada = malloc(sizeof(nodeCliente_t));
 			entrada->categoria = catTemp;
@@ -72,18 +79,74 @@ nodeCliente_t *initFilaEspera(){
 			temp->prox->tempo = tempoTemp;
 			temp->prox->prox = NULL;
 		}
+	*/
+		entrada = appendCliente(entrada, catTemp, tempoTemp);
+
 	}
 	return(entrada);
 }
 
-
 void printLista(nodeCliente_t *head){
-	nodeCliente_t *atual = head;
+	nodeCliente_t *cursor = head;
 
-	while(atual != NULL){
-		printf("CAT: %d | TEMPO: %d\n", atual->categoria, atual->tempo);
-		atual = atual->prox;
+	while(cursor != NULL){
+		printf("CAT: %d | TEMPO: %d\n", cursor->categoria, cursor->tempo);
+		cursor = cursor->prox;
 	}
+}
+
+nodeCliente_t *appendCliente(nodeCliente_t *head, int catTemp, int tempoTemp){
+	nodeCliente_t *cursor;
+	cursor = head;
+	
+	if(cursor == NULL){
+		printf("HEAD VAZIA. ENCHENDO\n");
+		
+		cursor = (nodeCliente_t*)malloc(sizeof(nodeCliente_t));
+		cursor->categoria = catTemp;
+		cursor->tempo = tempoTemp;
+		cursor->prox = NULL;
+	} else {
+		printf("RESTO DA LISTA AAAAA\n");
+		
+		nodeCliente_t *newNode;
+		newNode = (nodeCliente_t*)malloc(sizeof(nodeCliente_t));
+		
+		while(cursor->prox != NULL){
+			cursor = cursor->prox;
+		}
+		
+		//prepares new node for insertion
+		newNode->categoria = catTemp;
+		newNode->tempo = tempoTemp;
+		newNode->prox = NULL;
+		
+		//tail of current list, previously null, now receives the new node
+		cursor->prox = newNode;
+
+	}
+
+	return cursor;	
+	
+	/*
+	nodeCliente_t *cursor, *newNode;
+	
+	
+
+
+	cursor = head;
+	newNode = NULL;
+
+	while(cursor->prox != NULL){
+		cursor = cursor->prox;
+	}
+	
+	newNode = malloc(sizeof(nodeCliente_t));
+	newNode->categoria = catTemp;
+       	newNode->tempo = tempoTemp;
+	newNode->prox = NULL;
+
+	cursor->prox = newNode;*/
 }
 
 int rand_range(int min_n, int max_n){
